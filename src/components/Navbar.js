971b20803ff37a5380fc40354ba3c4cafbaba7f4
Chad96 <chadrackndalamba@gmail.com>
2024-07-22 08:24:56 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+function sayHi() {
+  return (
+    <div>
+      <h1>Hi React</h1>
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Password" />
+
+      <select>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+    </div>
+  );
+}
+export default sayHi;
